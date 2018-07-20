06934f78f9f8ac40228fd189a667c8cab5b50f43
Juan Manuel Serrano <juanmanuel.serrano@hablapps.com>
2018-07-20 11:00:37 +0200

    Generalise to Kleisli

--- src/main/scala/SurfaceGetter.scala
+++ src/main/scala/SurfaceGetter.scala
@@ -6,34 +6,31 @@ import shapeless._, ops.hlist._
 
 object SurfaceGetter{
 
-  trait Syntax{
-
-    implicit def readerView[E2, E1, T](r: Reader[E1, T])(implicit
-        nat: Reader[E1, ?] ~> Reader[E2, ?]): Reader[E2, T] =
-      nat(r)
-  }
-
   trait Implicits extends LPI{
-
     implicit def surfaceGetter_singleField[
-        E2, E1, LE2 <: HList](implicit
+        P[_], E2, E1, LE2 <: HList](implicit
         GE2: Generic.Aux[E2, LE2],
         S: Selector[LE2, E1]) =
-      λ[Reader[E1,?] ~> Reader[E2,?]]{
-        _.local((GE2.to _) andThen S.apply) // compose?
+      λ[Kleisli[P, E1,?] ~> Kleisli[P, E2,?]]{
+        _.local(GE2.to _ andThen S.apply)
       }
-
   }
 
   trait LPI{
-
     implicit def surfaceGetter_multipleField[
-        E2, E1, LE2 <: HList, LE1 <: HList](implicit
+        P[_], E2, E1, LE2 <: HList, LE1 <: HList](implicit
         GE2: Generic.Aux[E2, LE2],
         GE1: Generic.Aux[E1, LE1],
         S: SelectAll[LE2, LE1]) =
-      λ[Reader[E1,?] ~> Reader[E2,?]]{
-        _.local((GE2.to _) andThen S.apply andThen GE1.from)
+      λ[Kleisli[P, E1,?] ~> Kleisli[P, E2,?]]{
+        _.local(GE2.to _ andThen S.apply andThen GE1.from)
       }
+  }
+
+  trait Syntax{
+    implicit def readerView[P[_], E2, E1, T](
+        r: Kleisli[P, E1, T])(implicit
+        nat: Kleisli[P, E1, ?] ~> Kleisli[P, E2, ?]): Kleisli[P, E2, T] =
+      nat(r)
   }
 }
--- src/main/scala/SurfaceGetterK.scala
+++ src/main/scala/SurfaceGetterK.scala
@@ -5,13 +5,13 @@ import scalaz._
 import shapeless._, ops.record._
 
 trait SurfaceGetterK[E2, E1, K]{
-  val apply: Reader[E1, ?] ~> Reader[E2, ?]
+  def apply[P[_]]: Kleisli[P, E1, ?] ~> Kleisli[P, E2, ?]
 }
 
 object SurfaceGetterK extends SurfaceGetterImplicits{
 
   trait Syntax{
-    implicit class SurfaceGetterKOps[E1, T](r: Reader[E1, T]){
+    implicit class SurfaceGetterKOps[P[_], E1, T](r: Kleisli[P, E1, T]){
       def at[E2](k: Witness)(implicit R: SurfaceGetterK[E2, E1, k.T]) =
         R.apply(r)
 
@@ -23,14 +23,14 @@ object SurfaceGetterK extends SurfaceGetterImplicits{
 
 trait SurfaceGetterImplicits extends SurfaceGetterImplicitsLPI{
 
-  def apply[E1, E2, K](implicit R: SurfaceGetterK[E1, E2, K]) = R.apply
+  def apply[E1, E2, K](implicit R: SurfaceGetterK[E1, E2, K]) = R
 
   implicit def surfaceGetterK_singleField[
       E2, E1, K, LE2 <: HList](implicit
       GE2: LabelledGeneric.Aux[E2, LE2],
       S: Selector.Aux[LE2, K, E1]) =
     new SurfaceGetterK[E2, E1, K]{
-      val apply = λ[Reader[E1,?] ~> Reader[E2,?]]{
+      def apply[P[_]] = λ[Kleisli[P, E1, ?] ~> Kleisli[P, E2, ?]]{
         _.local((GE2.to _) andThen S.apply)
       }
     }
@@ -44,7 +44,7 @@ trait SurfaceGetterImplicitsLPI{
       GE1: Generic.Aux[E1, LE1],
       S: SelectAll.Aux[LE2, K, LE1]) =
     new SurfaceGetterK[E2, E1, K]{
-      val apply = λ[Reader[E1,?] ~> Reader[E2,?]]{
+      def apply[P[_]] = λ[Kleisli[P, E1, ?] ~> Kleisli[P, E2, ?]]{
         _.local((GE2.to _) andThen S.apply andThen GE1.from)
       }
     }
