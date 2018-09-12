7a6c06e43aca9f4e2683c00499d5aeb074fc0473
Jesús López-González <jesuslopezgonzalez@lavabit.com>
2018-09-12 11:24:28 +0200

    Replace Monocle's Lens by state monad morphisms (#5)
    * Replace monocle Lens by state monad morphism
    
    * Ops, can't remove monocle dependency (MGetter)
    
    * Removes unused import

--- src/main/scala/DeepLens.scala
+++ src/main/scala/DeepLens.scala
@@ -1,12 +1,11 @@
 package naturally
 
 import scalaz._, Scalaz._
-import shapeless._, labelled._
-import _root_.monocle.Lens
+import shapeless._
 
 trait DeepLens[S, Ctx <: HList] {
   type A
-  val value: Lens[S, A]
+  def apply(): Lens[S, A]
 }
 
 object DeepLens {
@@ -14,17 +13,19 @@ object DeepLens {
   type Aux[S, Ctx <: HList, A2] = DeepLens[S, Ctx] { type A = A2 }
 
   def apply[S, Ctx <: HList](implicit ln: DeepLens[S, Ctx]): Lens[S, ln.A] =
-    ln.value
+    ln()
 
   def apply[S, Ctx <: HList, A2](ln: Lens[S, A2]): Aux[S, Ctx, A2] =
-    new DeepLens[S, Ctx] { type A = A2; val value = ln }
+    new DeepLens[S, Ctx] { type A = A2; def apply() = ln }
 
   implicit def base[S]: Aux[S, HNil, S] =
-    apply(Lens.id)
+    apply(NaturalTransformation.refl[State[S, ?]])
 
   implicit def inductive[S, H, T <: HList, A, B](implicit
       hln: MkFieldLens.Aux[S, H, A],
       tln: Aux[A, T, B]): Aux[S, H :: T, B] =
-    apply(Lens(hln().get)(a => s => hln().set(s)(a)) composeLens tln.value)
+    apply(λ[State[A, ?] ~> State[S, ?]] { sa => 
+      State(s => sa(hln().get(s)).leftMap(hln().set(s)))
+    }.compose[State[B, ?]](tln()))
 }
 
--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -1,5 +1,17 @@
+import scalaz._
+
 package object naturally
-  extends SurfaceGetter.Syntax
-  with SurfaceGetterK.Syntax
-  with SurfaceLens.Syntax
-  with naturally.monocle.Getter.Implicits
+    extends SurfaceGetter.Syntax
+    with SurfaceGetterK.Syntax
+    with SurfaceLens.Syntax
+    with naturally.monocle.Getter.Implicits {
+
+  type Lens[S, A] = State[A, ?] ~> State[S, ?]
+
+  implicit class LensSyntax[S, A](ln: Lens[S, A]) {
+    def get(s: S): A = ln(State.get).eval(s)
+    def set(a: A)(s: S): S = ln(State.put(a)).exec(s)
+    def modify(f: A => A)(s: S): S = ln(State.modify(f)).exec(s)
+  }
+}
+
